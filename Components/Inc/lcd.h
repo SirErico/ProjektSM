65ffcb6388a4c54a37633319045486dbfab67d1d
SirErico <44581065+SirErico@users.noreply.github.com>
2024-01-21 14:10:38 +0100

    LCD updated Components/ folder
    Added lcd .c and .h files

--- Components/Inc/lcd.h
+++ Components/Inc/lcd.h
@@ -1,168 +1,139 @@
-/**
-  ******************************************************************************
-  * @file     : lcd.h
-  * @author   : Olivier Van den Eede [ https://github.com/4ilo/HD44780-Stm32HAL ]
-  * @author   : AW    Adrian.Wojcik@put.poznan.pl
-  * @version  : 3.0
-  * @date     : 10-Jun-2018
-  * @brief    : Simple HD44780 driver library for STM32F7.
-  *             NOTE!: This code provides only WRITE features, no READ features.
-  ******************************************************************************
-  */
-#ifndef INC_LCD_H_
-#define INC_LCD_H_
+/*
+ * lcd.h
+ *
+ *  Created on: 10/06/2018
+ *      Author: Olivier Van den Eede
+ */
 
-/* Config --------------------------------------------------------------------*/
-#define LCD_PRINTF_ENABLE
-#define LCD_USE_TIMER
+#ifndef LCD_H_
+#define LCD_H_
 
-//#define LCD20xN    // For 20xN LCDs
-#define LCD16xN     // For 16xN LCDs
+#include "stm32f7xx_hal.h"
+#include "string.h"
+#include "stdio.h"
+#include "main.h"
 
-#ifdef LCD16xN
-#define LCD_LINE_LEN 16
-#define LCD_LINE_BUF_LEN 17
-#else
-#define LCD_LINE_LEN 20
-#define LCD_LINE_BUF_LEN 21
-#endif
+// #define LCD20xN 		// For 20xN LCDs
+#define LCD16xN			// For 16xN LCDs
 
-/* Includes ------------------------------------------------------------------*/
-#include "dio.h"
+// For row start addresses
+extern const uint8_t ROW_16[];
+extern const uint8_t ROW_20[];
 
-#include <string.h>
-#include <stdio.h>
+/************************************** Command register **************************************/
+#define CLEAR_DISPLAY 0x01
 
-#ifdef LCD_PRINTF_ENABLE
-#include <stdarg.h>
-#endif
+#define RETURN_HOME 0x02
 
-/* Typedef -------------------------------------------------------------------*/
-#ifdef LCD_USE_TIMER
-#define LCD_TimerType TIM_HandleTypeDef*
-#else
-#define LCD_TimerType void*
-#endif
+#define ENTRY_MODE_SET 0x04
+#define OPT_S	0x01					// Shift entire display to right
+#define OPT_INC 0x02					// Cursor increment
 
-typedef enum {
-  LCD_4_BIT_MODE,
-  LCD_8_BIT_MODE
-} LCD_ModeTypeDef;
+#define DISPLAY_ON_OFF_CONTROL 0x08
+#define OPT_D	0x04					// Turn on display
+#define OPT_C	0x02					// Turn on cursor
+#define OPT_B 	0x01					// Turn on cursor blink
 
-typedef struct {
-  DIO_Handle_TypeDef* DATA;
-  DIO_Handle_TypeDef RS;
-  DIO_Handle_TypeDef E;
-  LCD_ModeTypeDef Mode;
-  LCD_TimerType Timer;
-  _Bool IsInitialized;
-} LCD_DIO_HandleTypeDef;
+#define CURSOR_DISPLAY_SHIFT 0x10		// Move and shift cursor
+#define OPT_SC 0x08
+#define OPT_RL 0x04
 
-/* Define --------------------------------------------------------------------*/
-#define LCD_CLEAR_DISPLAY 0x01
+#define FUNCTION_SET 0x20
+#define OPT_DL 0x10						// Set interface data length
+#define OPT_N 0x08						// Set number of display lines
+#define OPT_F 0x04						// Set alternate font
+#define SETCGRAM_ADDR 0x040
+#define SET_DDRAM_ADDR 0x80				// Set DDRAM address
 
-#define LCD_RETURN_HOME 0x02
 
-#define LCD_ENTRY_MODE_SET 0x04
-#define LCD_OPT_S   0x01        // Shift entire display to right
-#define LCD_OPT_INC 0x02        // Cursor increment
+/************************************** Helper macros **************************************/
+#define DELAY(X) HAL_Delay(X)
 
-#define LCD_DISPLAY_ON_OFF_CONTROL 0x08
-#define LCD_OPT_D  0x04         // Turn on display
-#define LCD_OPT_C  0x02         // Turn on cursor
-#define LCD_OPT_B  0x01         // Turn on cursor blink
 
-#define LCD_CURSOR_DISPLAY_SHIFT 0x10 // Move and shift cursor
-#define LCD_OPT_SC 0x08
-#define LCD_OPT_RL 0x04
-
-#define LCD_FUNCTION_SET    0x20
-#define LCD_OPT_DL          0x10    // Set interface data length
-#define LCD_OPT_N           0x08    // Set number of display lines
-#define LCD_OPT_F           0x04    // Set alternate font
-#define LCD_SETCGRAM_ADDR  0x040
-#define LCD_SET_DDRAM_ADDR  0x80    // Set DDRAM address
-
-#define LCD_NIB  4
+/************************************** LCD defines **************************************/
+#define LCD_NIB 4
 #define LCD_BYTE 8
-
-#define LCD_DATA_REG    1
+#define LCD_DATA_REG 1
 #define LCD_COMMAND_REG 0
 
-#define LCD_MENU_CURSOR_CHAR 0x7E // '->' (ROM Code: A00)
 
-/* Macro ---------------------------------------------------------------------*/
+/************************************** LCD typedefs **************************************/
+#define Lcd_PortType GPIO_TypeDef*
+#define Lcd_PinType uint16_t
 
-/* Public variables ----------------------------------------------------------*/
+/// Tryby pracy wyświetlacza.
+///
+/// Pozwala na wybranie pracy w trybie 4 lub 8 bitowym.
+typedef enum {
+	LCD_4_BIT_MODE,
+	LCD_8_BIT_MODE
+} Lcd_ModeTypeDef;
 
-/* Public function prototypes ------------------------------------------------*/
+/// Struktura opisująca wyświetlacz LCD
+///
+/// Zawiera informację o urzywanych pinach oraz trybie pracy.
+/// @param data_port Port zawierający Piny Danych.
+/// @param data_pin Ustawienie pinów danych wykorzystywanych do komunikacji z wyświetlaczem.
+/// @param rs_port Port zawierający pin Resetu.
+/// @param rs_pin Pin resetu.
+/// @param en_port Port zawierający pin Enable.
+/// @param en_pin Pin enable.
+typedef struct {
+	Lcd_PortType * data_port;
+	Lcd_PinType * data_pin;
 
-/**
- * @brief LCD initialization procedure.
- * @note Cursor off, Cursor increment on, No blink @see HD44780 technical note.
- * @param[in] hlcd : LCD handler
- * @return None 
- */
-void LCD_DIO_Init(LCD_DIO_HandleTypeDef* hlcd);
+	Lcd_PortType rs_port;
+	Lcd_PinType rs_pin;
 
-/**
- * @brief Write a decimal number on the current position.
- * @param[in] hlcd   : LCD handler
- * @param[in] number : Decimal number, max. 10 digits
- * @return None 
- */
-void LCD_DIO_printDecInt(LCD_DIO_HandleTypeDef* hlcd, int number);
+	Lcd_PortType en_port;
+	Lcd_PinType en_pin;
 
-/**
- * @brief Write a hexadecimal number on the current position.
- * @param[in] hlcd   : LCD handler
- * @param[in] number : Hexadecimal number, max. 10 digits
- * @return None 
- */
-void LCD_DIO_printHexInt(LCD_DIO_HandleTypeDef* hlcd, int number);
+	Lcd_ModeTypeDef mode;
 
-/**
- * @brief Write a string on the current position.
- * @param[in] hlcd : LCD handler
- * @param[in] str  : Null-terminated string
- * @return None 
- */
-void LCD_DIO_printStr(LCD_DIO_HandleTypeDef* hlcd, char* str);
+} Lcd_HandleTypeDef;
 
-/**
- * @brief Set the cursor position.
- * @param[in] hlcd : LCD handler
- * @param[in] row  : Display row (line): 0 to N
- * @param[in] col  : Display column: 0 to 15 (16 character display) or 19 (20 character display)
- * @return None 
- */
-void LCD_DIO_SetCursor(LCD_DIO_HandleTypeDef* hlcd, uint8_t row, uint8_t col);
 
-/**
- * @brief Clear the screen.
- * @param[in] hlcd : LCD handler
- * @return None 
- */
-void LCD_DIO_Clear(LCD_DIO_HandleTypeDef * lcd);
+/************************************** Public functions **************************************/
+///
 
-/**
- * @brief Clear the screen.
- * @param[in] hlcd   : LCD handler
- * @param[in] code   : Defined character code in display memory @see HD44780 technical note.
- * @param[in] bitmap : Defined character array @see HD44780 technical note.
- * @return None 
- */
-void LCD_DIO_DefineChar(LCD_DIO_HandleTypeDef* lcd, uint8_t code, uint8_t bitmap[]);
+///Inicjalizacja wyświetlacza.
+///
+///Ustawienie kursora, wyczyszczenie ekranu.
+///@param *lcd handler wyświetlacza.
+void Lcd_init(Lcd_HandleTypeDef * lcd);
+///Wypisanie liczby zmiennoprzecinkowej w miejscu kursora.
+///
+///@param *lcd handler wyświetlacza.
+///@param number liczba float do wyświetlenia.
+void Lcd_float(Lcd_HandleTypeDef * lcd, float number);
+///Wypisanie liczby całkowitej w miejscu kursora.
+///
+///@param *lcd handler wyświetlacza.
+///@param number liczba int do wyświetlenia.
+void Lcd_int(Lcd_HandleTypeDef * lcd, int number);
+///Wypisanie ciągu znaków w miejscu kursora.
+///
+///@param *lcd handler wyświetlacza.
+///@param string Tablica znaków do wyświetlenia.
+void Lcd_string(Lcd_HandleTypeDef * lcd, char * string);
+///Ustawienie kursora.
+///
+///@param *lcd handler wyświetlacza.
+///@param row Indeks rzędu.
+///@param col Indeks kolumny.
+void Lcd_cursor(Lcd_HandleTypeDef * lcd, uint8_t row, uint8_t col);
+///Inicjalizacja struktury wyświetlacza.
+///
+// UStawienie wykorzystywanych Pinów. Ustawienie trybu pracy.
+///@see Lcd_HandleTypeDef.
+Lcd_HandleTypeDef Lcd_create(
+		Lcd_PortType port[], Lcd_PinType pin[],
+		Lcd_PortType rs_port, Lcd_PinType rs_pin,
+		Lcd_PortType en_port, Lcd_PinType en_pin, Lcd_ModeTypeDef mode);
+void Lcd_define_char(Lcd_HandleTypeDef * lcd, uint8_t code, uint8_t bitmap[]);
+///Wyczyszczenie wyświetlacza.
+///
+///@param *lcd handler wyświetlacza.
+void Lcd_clear(Lcd_HandleTypeDef * lcd);
 
-#ifdef LCD_PRINTF_ENABLE
-/**
- * @brief Write text in standard format on the current position.
- * @param[in] hlcd   : LCD handler
- * @param[in] format : Text format @see http://www.cplusplus.com/reference/cstdio/printf/
- * @param[in] ...    : Variadic arguments
- * @return None
- */
-void LCD_DIO_printf(LCD_DIO_HandleTypeDef * lcd, const char* format, ...);
-#endif
-
-#endif /* INC_LCD_H_ */
+#endif /* LCD_H_ */
--- Components/Src/lcd.c
+++ Components/Src/lcd.c
@@ -1,261 +1,163 @@
-/**
-  ******************************************************************************
-  * @file     : lcd.c
-  * @author   : Olivier Van den Eede [ https://github.com/4ilo/HD44780-Stm32HAL ]
-  * @author   : AW    Adrian.Wojcik@put.poznan.pl
-  * @version  : 3.0
-  * @date     : 10-Jun-2018
-  * @brief    : Simple HD44780 driver library for STM32F7.
-  *             NOTE!: This code provides only WRITE features, no READ features.
-  ******************************************************************************
-  */
-  
-/* Includes ------------------------------------------------------------------*/
+/*
+ * lcd.c
+ *
+ *  Created on: 10/06/2018
+ *      Author: Olivier Van den Eede
+ */
+
 #include "lcd.h"
+const uint8_t ROW_16[] = {0x00, 0x40, 0x10, 0x50};
+const uint8_t ROW_20[] = {0x00, 0x40, 0x14, 0x54};
+/************************************** Static declarations **************************************/
 
-/* Typedef -------------------------------------------------------------------*/
-
-/* Define --------------------------------------------------------------------*/
-#define LCD_NUMBER_BUF_SIZE 21
-#define LCD_PRINTF_BUF_SIZE 64
-
-/* Macro ---------------------------------------------------------------------*/
-#ifdef LCD_USE_TIMER
-#define __lcd_dio_delay(__HANDLE__, delay_ms)  __lcd_dio_delay_us((__HANDLE__),(float)delay_ms * 1000.0)
-#else
-#define __lcd_dio_delay(__HANDLE__, delay_ms)  HAL_Delay((uint32_t)delay_ms);
-#endif
+static void lcd_write_data(Lcd_HandleTypeDef * lcd, uint8_t data);
+static void lcd_write_command(Lcd_HandleTypeDef * lcd, uint8_t command);
+static void lcd_write(Lcd_HandleTypeDef * lcd, uint8_t data, uint8_t len);
 
 
-/* Private variables ---------------------------------------------------------*/
-const uint8_t LCD_ROW_16[] = {0x00, 0x40, 0x10, 0x50};
-const uint8_t LCD_ROW_20[] = {0x00, 0x40, 0x14, 0x54};
-
-/* Public variables ----------------------------------------------------------*/
+/************************************** Function definitions **************************************/
 
 
-/* Private function ----------------------------------------------------------*/
-#ifdef LCD_USE_TIMER
-/**
- * @brief LCD delay function
- * @param[in] hlcd     : LCD handler
- * @param[in] delay_us : Delay period in microseconds
- * @return None
- */
-void __lcd_dio_delay_us(LCD_DIO_HandleTypeDef* hlcd, uint16_t delay_us)
+Lcd_HandleTypeDef Lcd_create(
+		Lcd_PortType port[], Lcd_PinType pin[],
+		Lcd_PortType rs_port, Lcd_PinType rs_pin,
+		Lcd_PortType en_port, Lcd_PinType en_pin, Lcd_ModeTypeDef mode)
 {
-  __HAL_TIM_SET_COUNTER(hlcd->Timer, 0);
-  HAL_TIM_Base_Start(hlcd->Timer);
-  while(__HAL_TIM_GET_COUNTER(hlcd->Timer) < delay_us);
-  HAL_TIM_Base_Stop(hlcd->Timer);
-}
-#endif
+	Lcd_HandleTypeDef lcd;
 
-/**
- * @brief Set len bits on the bus and toggle the enable line
- * @param[in] hlcd : LCD handler
- * @param[in] data : Data byte
- * @param[in] len  : Data port size (length): 4 ( LCD_NIB )or 8 ( LCD_BYTE ) bits
- * @return None
- */
-void __lcd_dio_write(LCD_DIO_HandleTypeDef* hlcd, uint8_t data, uint8_t len)
+	lcd.mode = mode;
+
+	lcd.en_pin = en_pin;
+	lcd.en_port = en_port;
+
+	lcd.rs_pin = rs_pin;
+	lcd.rs_port = rs_port;
+
+	lcd.data_pin = pin;
+	lcd.data_port = port;
+
+	Lcd_init(&lcd);
+
+	return lcd;
+}
+
+void Lcd_init(Lcd_HandleTypeDef * lcd)
 {
-  DIO_WriteHigh(&(hlcd->E));
+	if(lcd->mode == LCD_4_BIT_MODE)
+	{
+			lcd_write_command(lcd, 0x33);
+			lcd_write_command(lcd, 0x32);
+			lcd_write_command(lcd, FUNCTION_SET | OPT_N);				// 4-bit mode
+	}
+	else
+		lcd_write_command(lcd, FUNCTION_SET | OPT_DL | OPT_N);
 
-  for(uint8_t i = 0; i < len; i++)
-    DIO_Write(&(hlcd->DATA[i]), (data >> i) & 0x01);
 
-  DIO_WriteLow(&(hlcd->E)); // Data receive on falling edge
-  __lcd_dio_delay(hlcd, 0.05);  // > 41 us
+	lcd_write_command(lcd, CLEAR_DISPLAY);						// Clear screen
+	lcd_write_command(lcd, DISPLAY_ON_OFF_CONTROL | OPT_D);		// Lcd-on, cursor-off, no-blink
+	lcd_write_command(lcd, ENTRY_MODE_SET | OPT_INC);			// Increment cursor
 }
 
-/**
- * @brief Write a byte to the command register
- * @param[in] hlcd    : LCD handler
- * @param[in] command : Display command @see lcd.h/Define
- * @return None
- */
-void __lcd_dio_write_command(LCD_DIO_HandleTypeDef* hlcd, uint8_t command)
+void Lcd_float(Lcd_HandleTypeDef * lcd, float number)
 {
-  DIO_Write(&(hlcd->RS), LCD_COMMAND_REG);  // Write to command register
-
-  if(hlcd->Mode == LCD_4_BIT_MODE)
-  {
-    if(hlcd->IsInitialized) // Before initialization ignore most significant nibble
-    {
-      __lcd_dio_write(hlcd, (command >> 4), LCD_NIB);
-    }
-    __lcd_dio_write(hlcd, command & 0x0F, LCD_NIB);
-  }
-  else
-  {
-     __lcd_dio_write(hlcd, command, LCD_BYTE);
-  }
+	char buffer[11];
+	char buffer_out[11];
+	sprintf(buffer, "%f", number);
+	int period = 0;
+	for(period = 0; period<sizeof(buffer); period++)
+	{
+		if(buffer[period] == '.')
+			break;
+	}
+	strncpy(buffer_out, buffer, period + 4);
+	Lcd_string(lcd, buffer_out);
 }
-
-/**
- * @brief Write a byte to the data register
- * @param[in] hlcd : LCD handler
- * @param[in] data : Display data byte
- * @return None
- */
-void __lcd_dio_write_data(LCD_DIO_HandleTypeDef* hlcd, uint8_t data)
+void Lcd_int(Lcd_HandleTypeDef * lcd, int number)
 {
-  DIO_Write(&(hlcd->RS), LCD_DATA_REG);     // Write to data register
-
-  if(hlcd->Mode == LCD_4_BIT_MODE)
-  {
-    __lcd_dio_write(hlcd, data >> 4, LCD_NIB);
-    __lcd_dio_write(hlcd, data & 0x0F, LCD_NIB);
-  }
-  else
-  {
-    __lcd_dio_write(hlcd, data, LCD_BYTE);
-  }
+	char buffer[11];
+	sprintf(buffer, "%d", number);
+	Lcd_string(lcd, buffer);
 }
 
-/* Public function -----------------------------------------------------------*/
-
-/**
- * @brief LCD initialization procedure.
- * @note Cursor off, Cursor increment on, No blink @see HD44780 technical note.
- * @param[in] hlcd : LCD handler
- * @return None 
- */
-void LCD_DIO_Init(LCD_DIO_HandleTypeDef* hlcd)
+void Lcd_string(Lcd_HandleTypeDef * lcd, char * string)
 {
-  hlcd->IsInitialized = 0;
-
-  __lcd_dio_delay(hlcd, 15.2);         // >15 ms
-
-  if(hlcd->Mode == LCD_4_BIT_MODE)
-  {
-    __lcd_dio_write_command(hlcd, 0x3);  // 0011
-    __lcd_dio_delay(hlcd, 4.2);        // > 4.1 ms
-    __lcd_dio_write_command(hlcd, 0x3);  // 0011
-    __lcd_dio_delay(hlcd, 0.2);        // > 0.1 ms
-    __lcd_dio_write_command(hlcd, 0x3);  // 0011
-    __lcd_dio_write_command(hlcd, 0x2);  // 0010
-
-    hlcd->IsInitialized = 1;
-
-    __lcd_dio_write_command(hlcd, LCD_FUNCTION_SET | LCD_OPT_N);
-  }
-  else if(hlcd->Mode == LCD_8_BIT_MODE) /* TODO: test 8-bit interface */
-  {
-    __lcd_dio_write_command(hlcd, 0x30); // 0011 XXXX
-    __lcd_dio_delay(hlcd, 4.2);        // > 4.1 ms
-    __lcd_dio_write_command(hlcd, 0x30); // 0011 XXXX
-    __lcd_dio_delay(hlcd, 0.2);        // > 0.1 ms
-    __lcd_dio_write_command(hlcd, 0x30); // 0011 XXXX
-
-    hlcd->IsInitialized = 1;
-
-    __lcd_dio_write_command(hlcd, LCD_FUNCTION_SET | LCD_OPT_DL | LCD_OPT_N);
-  }
-
-  __lcd_dio_write_command(hlcd, LCD_CLEAR_DISPLAY);                      // Clear screen
-  __lcd_dio_delay(hlcd, 1.6);                                            // > 1.52 ms
-  __lcd_dio_write_command(hlcd, LCD_DISPLAY_ON_OFF_CONTROL | LCD_OPT_D); // LCD on, Cursor off, No blink
-  __lcd_dio_write_command(hlcd, LCD_ENTRY_MODE_SET | LCD_OPT_INC);       // Cursor increment on
+	for(uint8_t i = 0; i < strlen(string); i++)
+	{
+		lcd_write_data(lcd, string[i]);
+	}
 }
 
-/**
- * @brief Write a decimal number on the current position.
- * @param[in] hlcd   : LCD handler
- * @param[in] number : Decimal number, max. 10 digits
- * @return None 
- */
-void LCD_DIO_printDecInt(LCD_DIO_HandleTypeDef* hlcd, int number)
+
+void Lcd_cursor(Lcd_HandleTypeDef * lcd, uint8_t row, uint8_t col)
 {
-  char buffer[LCD_NUMBER_BUF_SIZE];
-  sprintf(buffer, "%d", number);
+	#ifdef LCD20xN
+	lcd_write_command(lcd, SET_DDRAM_ADDR + ROW_20[row] + col);
+	#endif
 
-  LCD_DIO_printStr(hlcd, buffer);
+	#ifdef LCD16xN
+	lcd_write_command(lcd, SET_DDRAM_ADDR + ROW_16[row] + col);
+	#endif
 }
 
-/**
- * @brief Write a hexadecimal number on the current position.
- * @param[in] hlcd   : LCD handler
- * @param[in] number : Hexadecimal number, max. 10 digits
- * @return None 
- */
-void LCD_DIO_printHexInt(LCD_DIO_HandleTypeDef* hlcd, int number)
+
+void Lcd_clear(Lcd_HandleTypeDef * lcd) {
+	lcd_write_command(lcd, CLEAR_DISPLAY);
+}
+
+void Lcd_define_char(Lcd_HandleTypeDef * lcd, uint8_t code, uint8_t bitmap[]){
+	lcd_write_command(lcd, SETCGRAM_ADDR + (code << 3));
+	for(uint8_t i=0;i<8;++i){
+		lcd_write_data(lcd, bitmap[i]);
+	}
+
+}
+
+
+/************************************** Static function definition **************************************/
+
+
+void lcd_write_command(Lcd_HandleTypeDef * lcd, uint8_t command)
 {
-  char buffer[LCD_NUMBER_BUF_SIZE];
-  sprintf(buffer, "%x", number);
+	HAL_GPIO_WritePin(lcd->rs_port, lcd->rs_pin, LCD_COMMAND_REG);		// Write to command register
 
-  LCD_DIO_printStr(hlcd, buffer);
+	if(lcd->mode == LCD_4_BIT_MODE)
+	{
+		lcd_write(lcd, (command >> 4), LCD_NIB);
+		lcd_write(lcd, command & 0x0F, LCD_NIB);
+	}
+	else
+	{
+		lcd_write(lcd, command, LCD_BYTE);
+	}
+
 }
 
-/**
- * @brief Write a string on the current position.
- * @param[in] hlcd : LCD handler
- * @param[in] str  : Null-terminated string
- * @return None 
- */
-void LCD_DIO_printStr(LCD_DIO_HandleTypeDef* hlcd, char* str)
+
+void lcd_write_data(Lcd_HandleTypeDef * lcd, uint8_t data)
 {
-  for(uint8_t i = 0; i < strlen(str); i++)
-    __lcd_dio_write_data(hlcd, str[i]);
+	HAL_GPIO_WritePin(lcd->rs_port, lcd->rs_pin, LCD_DATA_REG);			// Write to data register
+
+	if(lcd->mode == LCD_4_BIT_MODE)
+	{
+		lcd_write(lcd, data >> 4, LCD_NIB);
+		lcd_write(lcd, data & 0x0F, LCD_NIB);
+	}
+	else
+	{
+		lcd_write(lcd, data, LCD_BYTE);
+	}
+
 }
 
-/**
- * @brief Set the cursor position.
- * @param[in] hlcd : LCD handler
- * @param[in] row  : Display row (line): 0 to N
- * @param[in] col  : Display column: 0 to 15 (16 character display) or 19 (20 character display)
- * @return None 
- */
-void LCD_DIO_SetCursor(LCD_DIO_HandleTypeDef* hlcd, uint8_t row, uint8_t col)
+
+void lcd_write(Lcd_HandleTypeDef * lcd, uint8_t data, uint8_t len)
 {
-  #ifdef LCD20xN
-  __lcd_dio_write_command(hlcd, LCD_SET_DDRAM_ADDR + LCD_ROW_20[row] + col);
-  #endif
+	for(uint8_t i = 0; i < len; i++)
+	{
+		HAL_GPIO_WritePin(lcd->data_port[i], lcd->data_pin[i], (data >> i) & 0x01);
+	}
 
-  #ifdef LCD16xN
-  __lcd_dio_write_command(hlcd, LCD_SET_DDRAM_ADDR + LCD_ROW_16[row] + col);
-  #endif
+	HAL_GPIO_WritePin(lcd->en_port, lcd->en_pin, 1);
+	DELAY(1);
+	HAL_GPIO_WritePin(lcd->en_port, lcd->en_pin, 0); 		// Data receive on falling edge
 }
-
-/**
- * @brief Clear the screen.
- * @param[in] hlcd : LCD handler
- * @return None 
- */
-void LCD_DIO_Clear(LCD_DIO_HandleTypeDef* hlcd) {
-  __lcd_dio_write_command(hlcd, LCD_CLEAR_DISPLAY);
-}
-
-/**
- * @brief Clear the screen.
- * @param[in] hlcd   : LCD handler
- * @param[in] code   : Defined character code in display memory @see HD44780 technical note.
- * @param[in] bitmap : Defined character array @see HD44780 technical note.
- * @return None 
- */
-void LCD_DIO_DefineChar(LCD_DIO_HandleTypeDef* hlcd, uint8_t code, uint8_t bitmap[]){
-  __lcd_dio_write_command(hlcd, LCD_SETCGRAM_ADDR + (code << 3));
-  
-  for(uint8_t i=0; i < 8; ++i)
-    __lcd_dio_write_data(hlcd, bitmap[i]);
-}
-
-#ifdef LCD_PRINTF_ENABLE
-/**
- * @brief Write text in standard format on the current position.
- * @param[in] hlcd   : LCD handler
- * @param[in] format : Text format @see http://www.cplusplus.com/reference/cstdio/printf/
- * @param[in] ...    : Variadic arguments
- * @return None
- */
-void LCD_DIO_printf(LCD_DIO_HandleTypeDef* hlcd, const char* format, ...)
-{
-  char buffer[LCD_PRINTF_BUF_SIZE];
-  va_list args;
-  va_start(args, format);
-  vsprintf(buffer,format, args);
-  LCD_DIO_printStr(hlcd, buffer);
-  va_end(args);
-}
-#endif
