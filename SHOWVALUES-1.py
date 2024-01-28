f1e080022f40361251070708f39456731f70e792
BrudneDupsko <152406708+BrudneDupsko@users.noreply.github.com>
2024-01-28 14:22:30 +0100

    Skrypt w pythonie do logowania danych oraz graficzny interfejs użytkownika do ustawiania wartości referencyjnej

--- SHOWVALUES-1.py
+++ SHOWVALUES-1.py
@@ -1,43 +1,53 @@
-import math
-import serial  # pip install pyserial
-import numpy as np
-from time import sleep
-import time
+import cv2
+import serial
 import json
-import keyboard  # pip install keyboard
+import keyboard
+import time
 
 hSerial = serial.Serial('COM3', 115200, timeout=1, parity=serial.PARITY_NONE)
-sleep(0.5)
+rpm = 0  # Zmienna do przechowywania wartości suwaka
+
 timestr = time.strftime("%Y%m%d-%H%M%S")
-hFile = open("data_two_position_controller_%s.txt" % (timestr), "a")
+hFile = open(f"data_two_positioncontroller{timestr}.txt", "a")
 
-hSerial.reset_input_buffer()
-hSerial.flush()
-run = True
+def on_trackbar_change(value):
+    global rpm
+    rpm = value
+    send_rpm = rpm + 1000  # Wartość, którą będziemy wysyłać przez port szeregowy
+    send_data = f"{send_rpm:04}"  # Formatujemy wartość do 4 znaków
+    hSerial.write(send_data.encode('utf-8'))  # Wysyłamy zaktualizowaną wartość przez port szeregowy
+    print(f"Sent RPM value: {send_rpm}")
 
+def main():
+    cv2.namedWindow('Manage RPM')
+    cv2.createTrackbar('RPM 1000+', 'Manage RPM', rpm, 2000, on_trackbar_change)
 
+    while True:
+        if keyboard.is_pressed("q"):
+            break
 
-while run:
-    text = hSerial.readline()
-    textString = text.decode("utf-8")
-    print(textString)
+        time.sleep(0.1)  # Dodajemy opóźnienie przed odczytem danych z portu szeregowego
 
+        received_data = hSerial.readline().decode("utf-8").strip()
+       # print(f"Received data: {received_data}")
 
+        try:
+            sample = json.loads(received_data)
+        except ValueError:
+            print("Bad JSON")
+            print(f"{received_data}\r")
+            continue
 
-    try:
-        sample = json.loads(text)
-        temperatureAnalog = sample["RPM"]
-        hFile.write("%s" % textString)
-    except ValueError:
-        print("Bad JSON")
-        print("%s\r" % {text})
-        hSerial.flush()
-        hSerial.reset_input_buffer()
+        received_rpm = sample.get("RPM", 0)
+        received_rpm_ref = sample.get("RPM_REF", 0)
+        received_duty = sample.get("Duty", 0)
 
+        hFile.write(f"{received_data}\n")
 
-    if keyboard.is_pressed("q"):
-        run = False  # finishing the loop
-        break
+        cv2.waitKey(10)
 
+    cv2.destroyAllWindows()
+    hFile.close()
+
+main()
 hSerial.close()
-hFile.close()
